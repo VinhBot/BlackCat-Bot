ae50e7b2ed7300874f96ad38024fddd139b6c2c3
VinhBot <11328657-VinhBot@users.noreply.replit.com>
2023-03-29 18:47:46 +0000

    cập nhật thay đổi (Thay đổi vị trí các tệp)

--- Assets/Sounds/main.js
+++
@@ -1,5 +0,0 @@
-const sounds = [
-  { huuda: require("./70tuoi.mp3") }
-];
-
-module.exports = spunds;
--- Commands/SlashCommands/Voice/voice.js
+++ Commands/SlashCommands/Voice/voice.js
@@ -42,7 +42,7 @@ module.exports = {
 			connection.destroy();
 		 });
      if(toggle === "1") {
-        player.play(createAudioResource(`${process.cwd()}/Events/Sounds/70tuoi.mp3`));
+        player.play(createAudioResource(`${process.cwd()}/Assets/Sounds/70tuoi.mp3`));
         interaction.reply({ content: "đang chạy sounds 70 tuổi" });
      } else if(toggle === "2") {
         interaction.reply({ content: "chỉ có 70 tuổi mới có sounds thôi khà khà" });
